d2e90ae7b924a5c7747a7fafa6a82359dbe640e8
gavinleehau <haule.it.ctu@gmail.com>
2021-09-08 11:07:49 +0700

    restaurant

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿ $(function(){
+    khoimonan = $('.nhieumon').isotope({
+        itemSelector: '.motmon',
+        layoutMode: 'masonry'
+    });
+    
+    khoimonan.imagesLoaded().progress( function() {
+        khoimonan.isotope('layout');
+    });
+
+    $('.tieudect a').click(function(){
+        dulieu = $(this).data('monan'); // lay du lieu ve data('abc')
+        // console.log(dulieu); khối này để check trong console
+        khoimonan.isotope({filter: dulieu})
+        return false; //vô hiệu hóa thẻ a link
+    })
+
+    // new WOW().init();
+})
+$(function(){
+  new WOW().init(); 
+});
